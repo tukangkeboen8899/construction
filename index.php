6b877bf1d0def82ff49087d8072897dbda4ec038
Suwandi <tukang@keboen23.com>
2021-11-01 15:05:00 +0700

    add htaccess

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
-<title>TITLE</title>
+<title>Dealdeal Experience</title>
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta charset="UTF-8">
